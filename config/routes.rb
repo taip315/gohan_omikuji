c034e31dd3fffbe40631657c9b6127eb6c3e4250
taip315 <beaeaeat@gmail.com>
2021-01-30 09:02:28 +0900

    おみくじ機能、トップへの遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: "restaurants#index"
-  resources :restaurants
+  resources :restaurants do
+    collection do
+      get :result
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
